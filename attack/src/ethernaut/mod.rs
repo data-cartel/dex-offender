e8db1a5c937c2aeca1eac8b739688b2146baf977
0xgleb <gleb.dianov@protonmail.com>
2023-09-03 18:15:30 +0200

    solve ethernaut lvl 16

--- attack/src/ethernaut/mod.rs
+++ attack/src/ethernaut/mod.rs
@@ -13,6 +13,7 @@ pub mod hack12_privacy;
 pub mod hack13_gatekeeper_one;
 pub mod hack14_gatekeeper_two;
 pub mod hack15_naught_coin;
+pub mod hack16_preservation;
 
 #[cfg(test)]
 mod tests {
@@ -34,6 +35,7 @@ mod tests {
     use hack13_gatekeeper_one as hack13;
     use hack14_gatekeeper_two as hack14;
     use hack15_naught_coin as hack15;
+    use hack16_preservation as hack16;
 
     #[tokio::test]
     async fn test() -> eyre::Result<()> {
@@ -57,6 +59,7 @@ mod tests {
         ctf::check_exploit(&roles, hack13::Exploit).await?;
         ctf::check_exploit(&roles, hack14::Exploit).await?;
         ctf::check_exploit(&roles, hack15::Exploit).await?;
+        ctf::check_exploit(&roles, hack16::Exploit).await?;
 
         Ok(())
     }
--- ctf/contracts/ethernaut/lvl16/Preservation.sol
+++ ctf/contracts/ethernaut/lvl16/Preservation.sol
@@ -13,8 +13,8 @@ pragma solidity ^0.8.0;
  * Things that might help
  *
  * > Look into Solidity's documentation on the delegatecall low level function,
- * how it works, how it can be used to delegate operations to on-chain.
- * libraries, and what implications it has on execution scope.
+ *     how it works, how it can be used to delegate operations to on-chain.
+ *     libraries, and what implications it has on execution scope.
  * > Understanding what it means for delegatecall to be context-preserving.
  * > Understanding how storage variables are stored and accessed.
  * > Understanding how casting works between different data types.
